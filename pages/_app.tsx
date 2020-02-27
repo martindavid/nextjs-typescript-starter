0df5eb6438e57f21771f16c1f16782a54b5e4077
Martin David Valentino <martin.valentino@cultureamp.com>
2020-02-27 22:20:14 +1100

    Update to the project
    - Bump npm packages to the latest version
    - Adjust the code structure to follow the latest convention for Next
      project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,33 @@
 import App from "next/app";
+import Head from "next/head";
 import Router from "next/router";
 import NProgress from "nprogress";
 
 export default class CustomApp extends App {
-  componentDidMount() {
-    Router.events.on("routeChangeComplete", () => {
-      NProgress.start();
-    });
+	componentDidMount() {
+		Router.events.on("routeChangeComplete", () => {
+			NProgress.start();
+		});
 
-    Router.events.on("routeChangeComplete", () => NProgress.done());
-    Router.events.on("routeChangeError", () => NProgress.done());
-  }
+		Router.events.on("routeChangeComplete", () => NProgress.done());
+		Router.events.on("routeChangeError", () => NProgress.done());
+	}
 
-  componentDidCatch(error: any, errorInfo: any) {
-    console.log(error);
-    super.componentDidCatch(error, errorInfo);
-  }
+	componentDidCatch(error: any, errorInfo: any) {
+		console.log(error);
+		super.componentDidCatch(error, errorInfo);
+	}
 
-  render() {
-    const { Component, pageProps } = this.props;
+	render() {
+		const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
-  }
+		return (
+			<>
+				<Head>
+					<title>Flask, React App</title>
+				</Head>
+				<Component {...pageProps} />;
+			</>
+		);
+	}
 }
